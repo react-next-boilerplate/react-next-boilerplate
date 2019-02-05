a8f3f0cf26590130ad2f76fad9ef5691e60c2552
Jorge Luis Calleja <jorgelca1205@gmail.com>
2019-02-04 22:59:19 -0600

    fead: Add initial config

---
+++ app/pages/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './styles.scss';
+
+class Index extends React.PureComponent {
+  render() {
+    return (
+      <div className={styles.root}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis modi
+        unde, sed nihil sapiente numquam ipsam earum rem blanditiis totam
+        aperiam error, assumenda vel adipisci. Unde velit aliquid autem ratione.
+      </div>
+    );
+  }
+}
+
+export default Index;
