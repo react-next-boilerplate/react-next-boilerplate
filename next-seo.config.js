3fa82fdd91cd9bf2589e00a930aff19f407607c5
Jorge Luis Calleja <jorgelca1205@gmail.com>
2019-10-11 17:47:45 -0500

    [SeoConfig]: Add config seo

---
+++ next-seo.config.js
@@ -0,0 +1,26 @@
+export default {
+  title: 'React Next Boilerplate',
+  description:
+    'A basis for reducing the configuration of your projects with nextJS, best development practices and popular libraries in the developer community.',
+  openGraph: {
+    url: 'https://github.com/react-next-boilerplate/react-next-boilerplate',
+    title: 'Chakra UI',
+    description:
+      'A basis for reducing the configuration of your projects with nextJS, best development practices and popular libraries in the developer community.',
+    images: [
+      {
+        url:
+          'https://raw.githubusercontent.com/react-next-boilerplate/react-next-boilerplate/master/docs/images/Preview.png',
+        width: 1012,
+        height: 506,
+        alt: 'React Next Boilerplate',
+      },
+    ],
+    site_name: 'React Next Boilerplate',
+  },
+  twitter: {
+    handle: '@wootsbot',
+    site: '@wootsbot',
+    cardType: 'summary_large_image',
+  },
+};
