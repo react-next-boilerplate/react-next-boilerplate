cbd42a182eaf7ed9037c0827a9882282ad7d17e1
Jorge Luis Calleja Alvarado <jorgelca1205@gmail.com>
2020-01-29 21:38:55 -0600

    Browserconfig (#39)
    * [browserconfig]: Add some favicon versions
    
    * [browserconfig]: Add config browser to document page

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -26,7 +26,18 @@ class MyDocument extends Document {
             }
           `}
         />
-        <Head />
+
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/static/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#000000" />
+          <link rel="shortcut icon" href="/static/favicon/favicon.png" />
+          <meta name="msapplication-config" content="/static/favicon/browserconfig.xml" />
+          <meta name="theme-color" content="#000" />
+        </Head>
+
         <body>
           <Main />
           <NextScript />
