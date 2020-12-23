95df5354ad2b5240db65c2b737099b1a3fb1df89
Jorge Luis Calleja Alvarado <jorgelca1205@gmail.com>
2020-12-22 23:20:06 -0600

    [rfc]: Add emotion 11

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 import { Provider } from 'react-redux';
-import { CacheProvider } from '@emotion/core';
-import { cache } from 'emotion';
 import Head from 'next/head';
 import App from 'next/app';
 
 import withReduxStore from 'utils/with-redux-store';
 import { appWithTranslation } from 'utils/with-i18next';
 
-import 'typeface-metropolis';
+import 'fontsource-metropolis';
 import '@typefaces-pack/typeface-inter';
 
 class Srr extends App {
@@ -22,9 +20,7 @@ class Srr extends App {
         </Head>
 
         <Provider store={reduxStore}>
-          <CacheProvider value={cache}>
-            <Component {...pageProps} />
-          </CacheProvider>
+          <Component {...pageProps} />
         </Provider>
       </React.StrictMode>
     );
